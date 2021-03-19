d139e1b1371d2b8a97118ae3843339aa4cd598f8
Niko Jovan <q@qq>
2021-03-19 18:32:44 -0400

    add simple api tests

--- call.js
+++ call.js
@@ -1,41 +1,87 @@
-﻿let url = 'http://localhost:5544/kvs/testcall';
+﻿const key = 'testKey';
+let url = 'http://localhost:5544/kvs/' + key;
 
 const http = require('http');
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 
-// testcall GET
-http.get(url, (res) => {
-  res.on('data', (chunk)=>{
-    console.log('response with data received');
-    console.log(chunk.toString());
-  })
-});
-
-
-
-// testcall PUT
-const data = '=valueHere';
-
-const options = {
-  hostname: 'localhost',
-  port: 5544,
-  path: '/kvs/testcall',
-  method: 'PUT',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-  }
-};
-
-const req = http.request(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  res.on('data', (data) => {
-    process.stdout.write(data);
+async function testGET() {
+  http.get(url, (res) => {
+    console.log('GET, statusCode:', res.statusCode);
+    res.on('data', (chunk)=>{
+      console.log(chunk.toString());
+    })
   });
-});
+  await delay(5000);
+}
 
-req.on('error', (e) => {
-  console.error(e);
-});
+async function testPUT() {
+  const data = '=Well, hereComesTheString(data)...';
 
-req.write(data)
-req.end();
+  const options = {
+    hostname: 'localhost',
+    port: 5544,
+    path: '/kvs/' + key,
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+  };
+
+  const req = http.request(options, (res) => {
+    console.log('PUT, statusCode:', res.statusCode);
+    res.on('data', async (data) => {
+      process.stdout.write(data);
+    });
+  });
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  req.write(data)
+  req.end();
+  
+  await delay(8000);
+}
+
+async function testDELETE() {
+  const options = {
+    hostname: 'localhost',
+    port: 5544,
+    path: '/kvs/' + key,
+    method: 'DELETE'
+  };
+
+  const req = http.request(options, (res) => {
+    console.log('DELETE, statusCode:', res.statusCode);
+    res.on('data', async (data) => {
+      process.stdout.write(data);
+    });
+  });
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  req.end();
+  
+  await delay(5000);
+}
+
+
+async function runTestCalls() {
+  console.log('testcall GET, should not find anything:');
+  await testGET();
+  console.log('*****');
+  console.log('testcall PUT, should always succeed:');
+  await testPUT();
+  console.log('*****');
+  console.log('testcall GET, now it should find entry:');
+  await testGET();
+  console.log('*****');
+  console.log('testcall DELETE, should always succeed:');
+  await testDELETE();
+  console.log('*****');
+  console.log('testcall GET, cannot find entry anymore:');
+  await testGET();
+  console.log('*****');
+}
+
+runTestCalls();
